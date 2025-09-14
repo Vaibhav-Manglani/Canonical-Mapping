5ca8639811e21e751beaaa0b6c8b544cfa951d6e
VeyMars <87473254+Vaibhav-Manglani@users.noreply.github.com>
2025-09-14 22:06:36 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,18 +1,18 @@
 import os
 
 # Google Gemini API Configuration
-# GOOGLE_API_KEY = "AIzaSyDCTicTU1DkVOlq9Niyl_i76MXI7MkTwto"
+# GOOGLE_API_KEY = "your-google-api-key"
 
 # LangChain API Configuration
-LANGCHAIN_API_KEY = "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"
+LANGCHAIN_API_KEY = "your-langchain-api-key"
 
 # Set environment variables
 
 
 def setup_environment():
     """Setup environment variables for API keys"""
-    # if GOOGLE_API_KEY != "AIzaSyDCTicTU1DkVOlq9Niyl_i76MXI7MkTwto":
+    # if GOOGLE_API_KEY != "your-google-api-key":
     #     os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
-    if LANGCHAIN_API_KEY != "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":
+    if LANGCHAIN_API_KEY != "your-langchain-api-key":
         os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
